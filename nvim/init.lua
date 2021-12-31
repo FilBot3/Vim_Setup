1924ee991f9330bd35764edf3d95dffe94302d7e
Phillip Dudley <Predatorian3@gmail.com>
2021-12-31 13:21:39 -0600

    Added some Neovim LSP Configs.

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,6 +50,7 @@ nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
 ]], true)
 
+-- Telescope Configurations
 require('telescope').setup({
   defaults = {
     layout_config = {},
@@ -59,3 +60,13 @@ require('telescope').setup({
   extensions = {},
   -- other configuration values here
 })
+
+-- nvim-lspconfig.
+-- Requires NodeJS and npm to be available in your PATH.
+-- @see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.eslint.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.yamlls.setup{}
