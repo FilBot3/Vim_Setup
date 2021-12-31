fb6b3cb82c7c58f75b5eb008d642d68014d4f9c2
Phillip Dudley <Predatorian3@gmail.com>
2021-12-31 13:28:50 -0600

    Trying out toggling the error diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,8 @@ require('telescope').setup({
 -- Requires NodeJS and npm to be available in your PATH.
 -- @see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
+vim.lsp.diagnostic.show_line_diagnostics()
+
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.eslint.setup{}
